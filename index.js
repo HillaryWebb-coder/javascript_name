9e0b5198e18067ffb684d2e72cadd3594c5a4e6a
Hillary Ogieleguea <hillariosky123@gmail.com>
2022-06-22 12:38:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fullname = 'Ogieleguea Hillary'
 const height = '7ft'
 const country = 'Nigeria'
 
-console.log(fullname)
-console.log(height)
-console.log(country)
+console.log(`Name: ${fullname}`)
+console.log(`Height: ${height}`)
+console.log(`Country: ${country}`)
